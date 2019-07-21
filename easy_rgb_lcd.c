72edeeadb69f13f0bb6e3ded5b8940de8992dc09
mahmood-ul-hassan <43936942+mahmood-ul-hassan@users.noreply.github.com>
2019-07-21 22:05:26 +0500

    Update easy_rgb_lcd.c

--- easy_rgb_lcd.c
+++ easy_rgb_lcd.c
@@ -3,7 +3,7 @@
  *
  *  Created on: Jul 10, 2019
  *  Author: @ArduinoEasy
- *	Github:	
+ *	Github:	https://github.com/mahmood-ul-hassan/Grove_LCD_RGB_Backlight
  *	Source: Based on SeeedStudio Arduino Library 
  *			(https://github.com/Seeed-Studio/Grove_LCD_RGB_Backlight)
  *
